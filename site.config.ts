66c73549f8eabcca40d6156890360c5e9d0297a2
Eternity-Sky <tengyumo666@hotmail.com>
2025-01-28 10:58:58 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '1886be6bc2cf80488c21ddd7aa86a6d2',
+  rootNotionPageId: '1896be6bc2cf812da7fae131d8702aad',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
